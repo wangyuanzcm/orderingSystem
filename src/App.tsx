e320f44baf988bac9073fec6e51f76e7424f01ee
wangyuanzcm <48780074+wangyuanzcm@users.noreply.github.com>
2023-12-21 14:28:37 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  ShowGuesser,
+} from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { authProvider } from "./authProvider";
+
+export const App = () => (
+  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Resource
+      name="商品列表"
+      list={ListGuesser}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+  </Admin>
+);
