9bcc2a0588a9d23dcc66965c87279975b76a4a86
wangyuanzcm <48780074+wangyuanzcm@users.noreply.github.com>
2024-01-03 10:17:09 +0800

    feat: 改造收件人信息页面

--- src/views/receiverinfo/columns.tsx
+++ src/views/receiverinfo/columns.tsx
@@ -1,29 +1,13 @@
-// import { debounce } from 'lodash-es';
-// import { Tag } from 'ant-design-vue';
 import type { TableColumn } from '@/components/core/dynamic-table';
-import { waitTime } from '@/utils/common';
 
-const names = ['王路飞', '王大蛇', '李白', '刺客伍六七'];
+export type TableListItem = API.MenuListResultItem;
+export type TableColumnItem = TableColumn<TableListItem>;
 
 export const extendAreaCode = (areaCode) =>
   areaCode ? [areaCode.slice(0, 2), areaCode.slice(0, 4), areaCode] : [];
 
 export const collapseAreaCode = (areaCode) =>
   areaCode.length > 0 ? areaCode[areaCode.length - 1] : '';
-
-export const fetchStatusMapData = (keyword = '') => {
-  const data = [
-    {
-      label: '已售罄',
-      value: 0,
-    },
-    {
-      label: '热卖中',
-      value: 1,
-    },
-  ].filter((n) => n.label.includes(keyword));
-  return waitTime<LabelValueOptions>(2000, data);
-};
 
 export const getClothesByGender = (gender: number) => {
   if (gender === 1) {
@@ -54,24 +38,7 @@ export const getClothesByGender = (gender: number) => {
   return [];
 };
 
-export const tableData = Array.from({ length: 30 }).map((_, i) => {
-  const gender = ~~(Math.random() * 2);
-  return {
-    id: i + 1,
-    date: new Date().toLocaleString(),
-    name: names[~~(Math.random() * 4)],
-    clothes: getClothesByGender(gender)[~~(Math.random() * 2)].label,
-    price: ~~(Math.random() * 1000),
-    gender,
-    img: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    status: ~~(Math.random() * 2),
-  };
-});
-
-// 数据项类型
-export type ListItemType = (typeof tableData)[number];
-// 使用TableColumn<ListItemType> 将会限制dataIndex的类型，但换来的是dataIndex有类型提示
-export const columns: TableColumn<ListItemType>[] = [
+export const baseColumns: TableColumnItem[] = [
   {
     title: '昵称',
     dataIndex: 'nick_name',
@@ -90,32 +57,7 @@ export const columns: TableColumn<ListItemType>[] = [
     resizable: true,
     formItemProps: {
       component: 'Select',
-      componentProps: ({ formInstance, formModel, tableInstance }) => ({
-        options: [
-          {
-            label: '男',
-            value: 1,
-          },
-          {
-            label: '女',
-            value: 0,
-          },
-        ],
-        onChange() {
-          console.log('tableInstance', tableInstance?.reload());
-
-          // 根据当前选择的性别，更新衣服可选项
-          formInstance?.updateSchema({
-            field: 'clothes',
-            componentProps: {
-              options: getClothesByGender(formModel.gender),
-            },
-          });
-          formModel['clothes'] = undefined;
-        },
-      }),
     },
-    customRender: ({ record }) => ['女', '男'][record.gender],
   },
   {
     title: '所在地区',
@@ -173,15 +115,6 @@ export const columns: TableColumn<ListItemType>[] = [
       defaultValue: '',
       required: false,
     },
-  },
-  {
-    title: '操作',
-    dataIndex: 'operation',
-    align: 'center',
-    hideInSearch: true,
-    width: 300,
-    resizable: true,
-    fixed: 'right',
   },
 
   // {
--- src/views/receiverinfo/index.vue
+++ src/views/receiverinfo/index.vue
@@ -5,81 +5,134 @@
       <DynamicTable
         size="small"
         bordered
-        :data-request="loadData"
+        :data-request="loadTableData"
         :columns="columns"
         row-key="id"
         @resize-column="handleResizeColumn"
       >
         <template #toolbar>
+          <a-button type="primary" :disabled="!$auth('sys.menu.add')" @click="openMenuModal({})">
+            新增收货人信息
+          </a-button>
+        </template>
+        <!-- <template #toolbar>
           <a-typography-text>您已创建{{ total }}个收货地址</a-typography-text>
           <FormModal></FormModal>
-        </template>
+        </template> -->
       </DynamicTable>
     </Card>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { watch, reactive, ref, toRaw } from 'vue';
-
   import { message, Card } from 'ant-design-vue';
-  import FormModal from './form-modal.vue';
-  import { columns, tableData } from './columns';
+  import { baseColumns, type TableListItem, type TableColumnItem } from './columns';
+  import { useFormModal } from '@/hooks/useModal/useFormModal';
+  import { updateMenu, createMenu, deleteMenu } from '@/api/system/menu';
+  import { useReceiverSchemas } from './formSchemas';
+
   import { useTable, type OnChangeCallbackParams } from '@/components/core/dynamic-table';
   import { services } from '@/utils/request';
 
   const [messageApi, contextHolder] = message.useMessage();
   const [DynamicTable, dynamicTableInstance] = useTable();
-  const total = ref(0);
-  const loadData = async (
-    params,
-    onChangeParams: OnChangeCallbackParams,
-  ): Promise<API.TableListResult> => {
-    console.log('params', params);
-    console.log('onChangeParams', onChangeParams);
+  const [showModal] = useFormModal();
+
+  const openMenuModal = async (record: Partial<TableListItem>) => {
+    const [formRef] = await showModal({
+      modalProps: {
+        title: `${record.id ? '编辑' : '新增'}收货人信息`,
+        width: 700,
+        onFinish: async (values) => {
+          console.log('新增/编辑收货人信息', values);
+          // record.id && (values.menuId = record.id);
+          // if (values.type === 1 && values.viewPath?.length) {
+          //   values.viewPath = values.viewPath.join('/');
+          // }
+          // if (values.type === 2 && values.perms?.length) {
+          //   values.perms = values.perms.map((n) => n.join(':')).toString();
+          // }
+          // await (record.id ? updateMenu : createMenu)(values);
+          // dynamicTableInstance.reload();
+        },
+      },
+      formProps: {
+        labelWidth: 100,
+        schemas: useReceiverSchemas(),
+      },
+    });
+
+    // formRef?.updateSchema([
+    //   {
+    //     field: 'parentId',
+    //     componentProps: {
+    //       treeDefaultExpandedKeys: [-1].concat(record?.keyPath || []),
+    //       treeData: ref([{ id: -1, name: '一级菜单', children: menuTree.value }]),
+    //     },
+    //   },
+    // ]);
+
+    // formRef?.setFieldsValue({
+    //   ...record,
+    //   icon: record.icon ?? '',
+    //   perms: record.perms
+    //     ?.split(',')
+    //     .filter(Boolean)
+    //     .map((n) => n.split(':')),
+    //   viewPath: record.viewPath?.split('/'),
+    //   parentId: record.parentId ?? -1,
+    // });
+  };
+  const delRowConfirm = async (record: TableListItem) => {
+    await deleteMenu({ menuId: record.id });
+    dynamicTableInstance.reload();
+  };
+
+  const loadTableData = async (params): Promise<API.TableListResult> => {
     const result = await services.receiverControllerPage({
       ...params,
     });
     console.log(result, 'result');
     // @ts-ignore
     const { list, pagination } = result.data;
-    total.value = pagination.total;
-    return {
-      list,
-      pagination,
-    };
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({
-          list: tableData,
-          ...params,
-        });
-        // 手动设置搜索表单的搜索项
-        dynamicTableInstance?.getQueryFormRef()?.updateSchema?.([
-          {
-            field: 'price',
-            componentProps: {
-              options: [
-                {
-                  label: '0-199',
-                  value: '0-199',
-                },
-                {
-                  label: '200-999',
-                  value: '200-999',
-                },
-              ],
-            },
-          },
-        ]);
-      }, 500);
-    });
+
+    return { list };
   };
 
   const handleResizeColumn = (w, col) => {
     // console.log('w', w, col);
     col.width = w;
   };
+
+  const columns: TableColumnItem[] = [
+    ...baseColumns,
+    {
+      title: '操作',
+      width: 160,
+      dataIndex: 'ACTION',
+      hideInSearch: true,
+      align: 'center',
+      fixed: 'right',
+      actions: ({ record }) => [
+        {
+          label: '编辑',
+          auth: {
+            perm: 'sys.menu.update',
+            effect: 'disable',
+          },
+          onClick: () => openMenuModal(record),
+        },
+        {
+          label: '删除',
+          auth: 'sys.menu.delete',
+          popConfirm: {
+            title: '你确定要删除吗？',
+            onConfirm: () => delRowConfirm(record),
+          },
+        },
+      ],
+    },
+  ];
 </script>
 
 <style lang="less" scoped></style>
